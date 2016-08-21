0790a7600a70d2e426d6750806268f81bf3f0354
matabuntu <isatyawan@yahoo.com>
2016-08-21 22:49:50 +1000

    init

---
+++ example.js
@@ -0,0 +1,27 @@
+var mapId = 7022, layerId = 28098; 
+
+(function () {
+    var m, s = document.location.search, $ = giscloud.exposeJQuery();
+
+ 
+    m = (s && s.match && s.match(/mid\=(\d+)/i));
+    if (m && m[1]) {
+        mapId = parseInt(m[1], 10);
+    }
+
+   
+    m = (s && s.match && s.match(/lid\=(\d+)/i));
+    if (m && m[1]) {
+        layerId = parseInt(m[1], 10);
+    }
+
+    
+
+    $(function() {
+        $("body").append(
+            '<a href="https://github.com/giscloud/GIS-Cloud-Examples/">' + 
+            '<img style="position: absolute; top: 0; right: 0; border: 0;" ' +
+            'src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>');
+    });
+
+})();
